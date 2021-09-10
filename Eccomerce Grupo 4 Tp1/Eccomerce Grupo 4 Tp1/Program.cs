ba73e2067297392264888fd9e297e23ee603dae1
LautaroRDavinci <lautaro.ramos@davinci.edu.ar>
2021-09-10 18:29:02 -0300

    Inicio De proyecto

---
+++ Eccomerce Grupo 4 Tp1/Eccomerce Grupo 4 Tp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eccomerce_Grupo_4_Tp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
